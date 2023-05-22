ff313ff7b123f2c6e40ba465d45a201dc8151c20
Henrique <henrique.wang44@gmail.com>
2023-05-22 17:32:31 -0300

    s

--- vars/sendEmail.groovy
+++ vars/sendEmail.groovy
@@ -1,4 +1,4 @@
-def call(Map config = [:]) {
+def call(to, subject, body) {
     sh "to: ${config.to}"
     sh "subject: ${config.subject}"
     sh "body: ${config.body}"
--- vars/sendSuccessEmail.groovy
+++ vars/sendSuccessEmail.groovy
@@ -2,5 +2,5 @@ def call() {
     def message = "Build ${env.BUILD_NUMBER} status: SUCCESS. Please go to ${env.BUILD_URL} for more information."
     def recipient = "Pull Request Owner"
     def subject = "Build ${env.BUILD_NUMBER} status: SUCCESS"
-    sendEmail(to:"${recipient}", subject:"${subject}", body:"${message}")
+    sendEmail("${recipient}", "${subject}", "${message}")
 }
