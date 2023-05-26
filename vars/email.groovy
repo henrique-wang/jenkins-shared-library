16b903170a45fd665ba6917f7be4202b640566d6
Henrique <henrique.wang44@gmail.com>
2023-05-26 15:41:22 -0300

    Add recipient parameter

--- vars/email.groovy
+++ vars/email.groovy
@@ -4,16 +4,14 @@ def sendEmail(String to, String subject, String body) {
     sh "echo body: ${body}"
 }
 
-def sendFailureEmail(String stage) {
+def sendFailureEmail(String stage, String recipient) {
     def message = "Build ${env.BUILD_NUMBER} status: FAILED. Failed Stage: ${stage}. Please go to ${env.BUILD_URL} for more information."
-    def recipient = "Pull Request Owner"
     def subject = "Build ${env.BUILD_NUMBER} status: FAILED"
     sendEmail("${recipient}", "${subject}", "${message}")
 }
 
-def sendSuccessEmail() {
+def sendSuccessEmail(String recipient) {
     def message = "Build ${env.BUILD_NUMBER} status: SUCCESS. Please go to ${env.BUILD_URL} for more information."
-    def recipient = "Pull Request Owner"
     def subject = "Build ${env.BUILD_NUMBER} status: SUCCESS"
     sendEmail("${recipient}", "${subject}", "${message}")
 }
